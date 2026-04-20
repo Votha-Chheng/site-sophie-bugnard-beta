88a695ea3b1f236cfe90e1222152d34f1bfa961f
Votha-Chheng <votha_chheng@gmx.fr>
2026-04-20 10:41:05 +0200

    Proposition avec les nouvelles couleurs du logo

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,11 +4,9 @@ import PageTitle from "@/components/PageTitle"
 const MonLivrePage = () => {
   return (
     <BodyLayout className="min-h-screen">
-      <PageTitle className="-translate-x-1/2 left-1/2 top-12">
-        <section className="">
-          Liste des Publications
-        </section>
-      </PageTitle>
+      <section className="">
+        Liste des Publications
+      </section>
     </BodyLayout>
   )
 }
